a7d2b44f9ffea7f8703bb4eb695802b0577784d5
Cameron-Jade Mummery <cami.mummery@aexp.com>
2019-09-16 11:46:43 +0100

    7

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,12 +11,19 @@ public class Main {
         for (i = 1;  i <= 100; i++ ) {
 
 
-            if (i % 3 == 0 && i % 5 == 0) {
+            if (i % 3 == 0 && i % 5 == 0 && i % 7 == 0) {
+                System.out.println("FizzBuzzBang");
+
+            } else if (i % 3 == 0 && i % 5 == 0){
                 System.out.println("FizzBuzz");
+            } else if (i % 3 == 0 && i % 7 == 0) {
+                System.out.println("FizzBang");
             } else if (i % 3 == 0) {
                 System.out.println("Fizz");
             } else if (i % 5 == 0) {
                 System.out.println("Buzz");
+            } else if (i % 7 == 0) {
+                System.out.println("Bang");
             }
 
                 else {
