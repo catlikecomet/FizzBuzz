e663767672d560be4a5327a191e0af3938b2508b
Cameron-Jade Mummery <cami.mummery@aexp.com>
2019-09-16 14:20:56 +0100

    changing code for three, five, sev and elvn

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,52 @@ package com.company;
 public class Main {
 
 
-
-
     public static void main(String[] args) {
         int i = 0;
+        String three = "Fizz";
+        String five = "Buzz";
+        String sev = "Bang";
+        String elvn = "Bong";
+        String thirt = "Fezz";
 
-        for (i = 1;  i <= 100; i++ ) {
+        for (i = 1; i <= 106; i++) {
+
+            if (i % 3 == 0) {
+                System.out.println(three);
+            } else if (i % 5 == 0) {
+                System.out.println(five);
+            } else if (i % 7 == 0) {
+                System.out.println(sev);
+            } else if (i % 11 == 0) {
+                System.out.println(elvn);
+            } else {
+                System.out.println(i);
+            }
+                if (i % 3 == 0 && i % 5 == 0) {
+                    System.out.println(three+five);
+                } else if (i % 3 ==0 && i % 7 == 0) {
+                    System.out.println(three+sev);
+                } else if (i % 3 == 0 && i % 11 == 0) {
+                    System.out.println(elvn);
+                }
+                    if (i % 5 == 0 && i % 7 == 0) {
+                        System.out.println(five+sev);
+                    } else if ( i % 5 == 0 && i % 11 == 0) {
+                        System.out.println(elvn);
+                    }
+                        if (i % 7 == 0 && i % 11 == 0){
+                            System.out.println(elvn);
+                        }
+                            if (i % 3 == 0 && i % 5 == 0 && i % 7 == 0){
+                                System.out.println(three+five+sev);
+                            }else if (i % 3 == 0 && i % 7 == 0 && i % 11 == 0){
+                                System.out.println(elvn);
+                            }
+                                if (i % 5 == 0 && i % 7 == 0 && i % 11 == 0){
+                                    System.out.println(elvn);
+                                }
+        }
+        /*for (i = 1;  i <= 100; i++ ) {
 
 
             if (i % 3 == 0 && i % 5 == 0 && i % 7 == 0 && i % 11 == 0) {
@@ -39,5 +79,6 @@ public class Main {
         }
 
 
+    }*/
     }
 }
